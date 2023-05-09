b3ffa3b2058a87c832be5974b8397db6f9a35105
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-05-09 14:55:23 +0200

    feat: add Area and Shop models

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Area, Shop
+
+
+@admin.register(Area, Shop)
+class UniversalAdmin(admin.ModelAdmin):
+    def get_list_display(self, request):
+        return [field.name for field in self.model._meta.concrete_fields]
--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.db.models import QuerySet
 
-# Create your models here.
+from econet.model import SoftDeleteModel
+
+
+class Area(SoftDeleteModel):
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Area"
+        verbose_name_plural = "Areas"
+        ordering = ("name",)
+        table_prefix = "area"
+
+    def create_shop(self, name, description):
+        shop = Shop.objects.create(area=self, name=name, description=description)
+        return shop
+
+    def get_shops(self) -> QuerySet:
+        return self.shops.all()
+
+
+class Shop(SoftDeleteModel):
+    area = models.ForeignKey(
+        "locations.Area",
+        on_delete=models.CASCADE,
+        related_name="shops",
+        blank=False,
+        null=False,
+    )
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.name} | {self.area.name}"
+
+    class Meta:
+        verbose_name = "Shop"
+        verbose_name_plural = "Shops"
+        ordering = ("name",)
+        table_prefix = "shop"
