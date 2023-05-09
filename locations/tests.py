5f7497cc094b11e051d531f7a8b3dfe8eccbf079
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-05-09 16:47:57 +0200

    feat: add test for uppercase

--- locations/tests.py
+++ locations/tests.py
@@ -1,3 +1,26 @@
-# from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from locations.models import Area, Shop
+
+
+class ShopsTest(APITestCase):
+    def setUp(self) -> None:
+        area = Area(name="BelvedereSouth")
+        area.save()
+        self.area = area
+
+    def test_shops_saved_in_uppercase(self):
+        """
+        @description: Test shops saved in uppercase
+        @return:
+        """
+        url = reverse("shops")
+        data = {"area": self.area.id, "name": "Long Cheng Plaza"}
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        shop = Shop.objects.first()
+        # check if shop's name is uppercase
+        self.assertEqual(shop.name, shop.name.upper())
