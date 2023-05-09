d1b22c2a0222e9d234ebe1eb7e942c08c2bd4a00
Ngonidzashe Mangudya <imngonii@gmail.com>
2023-05-09 17:06:20 +0200

    feat: add test for shop creation

--- locations/tests.py
+++ locations/tests.py
@@ -5,31 +5,35 @@ from rest_framework.test import APITestCase
 from locations.models import Area, Shop
 
 
+class AreasTest(APITestCase):
+    def creates_area(self):
+        url = reverse("areas")
+        data = {"name": "Marondera"}
+        response = self.client.post(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
 class ShopsTest(APITestCase):
     def setUp(self) -> None:
-        area = Area(name="BelvedereSouth")
+        area = Area(name="Marondera")
         area.save()
         self.area = area
 
-    def test_shops_saved_in_uppercase(self):
-        """
-        @description: Test shops saved in uppercase
-        @return:
-        """
+    def creates_shop(self):
         url = reverse("shops")
         data = {"area": self.area.id, "name": "LongChengPlaza"}
         response = self.client.post(url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+    def test_shops_saved_in_uppercase(self):
+        url = reverse("shops")
+        data = {"area": self.area.id, "name": "LongChengPlaza"}
+        self.client.post(url, data, format="json")
         shop = Shop.objects.first()
         # check if shop's name is uppercase
         self.assertEqual(shop.name, shop.name.upper())
 
     def test_doesnt_allow_special_characters(self):
-        """
-        @description: Test shops doesn't allow special characters
-        @return:
-        """
         url = reverse("shops")
         data = {"area": self.area.id, "name": "Long Cheng Plaza"}
         response = self.client.post(url, data, format="json")
